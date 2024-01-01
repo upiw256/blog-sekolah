532c1c600f550bec4a7a0b72c2b23306497f5c69
upiw256 <bilqimlb@gmail.com>
2024-01-01 07:38:00 +0700

    fix dashboard home

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -29,17 +29,12 @@
       <div class="row">
         <div class="col-md-3 col-sm-6 col-12">
           <div class="info-box <?= $warna[rand(0, 4)] ?>">
-            <span class="info-box-icon"><i class="far fa-bookmark"></i></span>
+            <span class="info-box-icon"><i class="fa fa-id-card"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Bookmarks</span>
-              <span class="info-box-number">41,410</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-              </div>
-              <span class="progress-description">
-                70% Increase in 30 Days
+              <span class="info-box-text">Siswa</span>
+              <span class="info-box-number">
+                <?= number_format($siswa, 0, ',', '.') ?>
               </span>
             </div>
             <!-- /.info-box-content -->
@@ -49,17 +44,12 @@
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
           <div class="info-box <?= $warna[rand(0, 4)] ?>">
-            <span class="info-box-icon"><i class="far fa-user"></i></span>
+            <span class="info-box-icon"><i class="fa fa-users"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">41,410</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-              </div>
-              <span class="progress-description">
-                70% Increase in 30 Days
+              <span class="info-box-text">Kelas</span>
+              <span class="info-box-number">
+                <?= $kelas ?>
               </span>
             </div>
             <!-- /.info-box-content -->
@@ -69,17 +59,12 @@
         <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-12">
           <div class="info-box <?= $warna[rand(0, 4)] ?>">
-            <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
+            <span class="info-box-icon"><i class="fa fa-graduation-cap"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Events</span>
-              <span class="info-box-number">41,410</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-              </div>
-              <span class="progress-description">
-                70% Increase in 30 Days
+              <span class="info-box-text">PTK</span>
+              <span class="info-box-number">
+                <?= number_format($ptk, 0, '.', '.') ?>
               </span>
             </div>
             <!-- /.info-box-content -->
@@ -92,14 +77,9 @@
             <span class="info-box-icon"><i class="fas fa-comments"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Comments</span>
-              <span class="info-box-number">41,410</span>
-
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
-              </div>
-              <span class="progress-description">
-                70% Increase in 30 Days
+              <span class="info-box-text">Berita</span>
+              <span class="info-box-number">
+                <?= $news ?>
               </span>
             </div>
             <!-- /.info-box-content -->
@@ -113,7 +93,27 @@
         <button class="btn btn-primary swalDefaultSuccess syncButton"><i class="fa fa-sync-alt"></i> Syncron
           Dapodik</button>
       </div>
-      
+      <h1>News Update</h1>
+      <div class="row mt-3">
+        <?php foreach ($update as $item): ?>
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header  bg-gray">
+                <h3 class="card-title">
+                  <?= esc($item->title) ?>
+                </h3>
+              </div>
+              <div class="card-body">
+                <!-- Add content here based on your data fields -->
+                <p>
+                  <?= esc($item->content) ?>
+                </p>
+                <!-- Add more fields as needed -->
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div><!-- /.container-fluid -->
   </div>
   <!-- /.content -->
