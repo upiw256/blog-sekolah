4d427e40b078c114245fbf526b041637d381657f
upiw256 <bilqimlb@gmail.com>
2024-01-02 06:26:29 +0700

    edit news

--- app/Controllers/Data.php
+++ app/Controllers/Data.php
@@ -9,23 +9,13 @@ class Data extends BaseController
 {
     public function index()
     {
-        // $client = new Client();
-        // $headers = [
-        //     'x-Barrier' => 'margaasih',
-        //     // Tambahkan header sesuai kebutuhan
-        // ];
-        // $sekolah = $client->request('GET', 'http://103.229.14.238:3000/api/sekolah', ['headers' => $headers]);
-        // // $siswa = $client->request('GET', 'http://app.sman1margaasih.sch.id:8080/siswa');
-        // // $rombel = $client->request('GET', 'http://app.sman1margaasih.sch.id:8080/rombel');
-        // // $response = $client->request('GET', 'http://app.sman1margaasih.sch.id:8080/');
-
-        // $data = [
-        //     'sekolah' => json_decode($sekolah->getBody()),
-        //     // 'siswa'=>json_decode($sekolah->getBody()),
-        //     // 'rombel'=>json_decode($sekolah->getBody())
-        // ];
-
-        return view('layout/fix');
+        $data = [
+            'news' => $this->news->paginate(2),
+            'allNews' => $this->news->findAll(),
+            'pager' => $this->news->pager,
+            'sekolah' => $this->sekolah->first(),
+        ];
+        return view('layout/fix', $data);
 
     }
 }
